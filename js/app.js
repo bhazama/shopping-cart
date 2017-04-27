9eb07182c61f505a2386a1edc638e001aa3ba7c4
Brendan Hazama <brenhazama@gmail.com>
2017-04-27 13:53:26 -1000

    first img update

--- js/app.js
+++ js/app.js
@@ -20,20 +20,26 @@ for(var i = 0; i<shoppingList.length; i++){
   var prod1Price = document.getElementById("price1");
   prod1Price.innerHTML = shoppingList[0].price;
 
+  var prod1Img = document.getElementById("img1");
+  prod1Img.innerHTML = "<img src=https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/ae4ef8dc-b6c6-4b23-b2e7-9cbe0d169ea1.jpg._CB285945496__SR300,300_.jpg alt = finger brush id=img1";
+
 
   /***************************************************
   ITEM 2
   ****************************************************/
 
 
-  var prod1Name = document.getElementById("title2");
+  var prod2Name = document.getElementById("title2");
   prod1Name.innerHTML = shoppingList[1].product;
 
-  var prod1Description = document.getElementById("description2");
+  var prod2Description = document.getElementById("description2");
   prod1Description.innerHTML = shoppingList[1].description;
 
-  var prod1Price = document.getElementById("price2");
+  var prod2Price = document.getElementById("price2");
   prod1Price.innerHTML = shoppingList[1].price;
+
+  var prod2Img = document.getElementById("img2");
+  
 
   
   /***************************************************
@@ -41,14 +47,16 @@ for(var i = 0; i<shoppingList.length; i++){
   ****************************************************/
   
 
-  var prod1Name = document.getElementById("title3");
+  var prod3Name = document.getElementById("title3");
   prod1Name.innerHTML = shoppingList[2].product;
 
-  var prod1Description = document.getElementById("description3");
+  var prod3Description = document.getElementById("description3");
   prod1Description.innerHTML = shoppingList[2].description;
 
-  var prod1Price = document.getElementById("price3");
+  var prod3Price = document.getElementById("price3");
   prod1Price.innerHTML = shoppingList[2].price;
+
+  var prod3Img = document.getElementById("img3");
 
   
 
@@ -57,14 +65,16 @@ for(var i = 0; i<shoppingList.length; i++){
   ****************************************************/
 
 
-  var prod1Name = document.getElementById("title4");
+  var prod4Name = document.getElementById("title4");
   prod1Name.innerHTML = shoppingList[3].product;
 
-  var prod1Description = document.getElementById("description4");
+  var prod4Description = document.getElementById("description4");
   prod1Description.innerHTML = shoppingList[3].description;
 
-  var prod1Price = document.getElementById("price4");
+  var prod4Price = document.getElementById("price4");
   prod1Price.innerHTML = shoppingList[3].price;
+
+  var prod4Img = document.getElementById("img4");
 
 
    /***************************************************
@@ -72,14 +82,16 @@ for(var i = 0; i<shoppingList.length; i++){
   ****************************************************/
 
   
-  var prod1Name = document.getElementById("title5");
+  var prod5Name = document.getElementById("title5");
   prod1Name.innerHTML = shoppingList[4].product;
 
-  var prod1Description = document.getElementById("description5");
+  var prod5Description = document.getElementById("description5");
   prod1Description.innerHTML = shoppingList[4].description;
 
-  var prod1Price = document.getElementById("price5");
+  var prod5Price = document.getElementById("price5");
   prod1Price.innerHTML = shoppingList[4].price;
+
+  var prod5Img = document.getElementById("img5");
 
  
  /***************************************************
@@ -87,14 +99,16 @@ for(var i = 0; i<shoppingList.length; i++){
   ****************************************************/
 
 
-  var prod1Name = document.getElementById("title6");
+  var prod6Name = document.getElementById("title6");
   prod1Name.innerHTML = shoppingList[5].product;
 
-  var prod1Description = document.getElementById("description6");
+  var prod6Description = document.getElementById("description6");
   prod1Description.innerHTML = shoppingList[5].description;
 
-  var prod1Price = document.getElementById("price6");
+  var prod6Price = document.getElementById("price6");
   prod1Price.innerHTML = shoppingList[5].price;
+
+  var prod6Img = document.getElementById("img6");
    
 }
 
